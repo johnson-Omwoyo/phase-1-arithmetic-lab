5628fa45e2bc07f03a96ed52c0c6bb8a6fc9a9ca
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-06-26 12:38:41 +0300

    Math methods

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10 + 1);
+let num3 = multiply + 4;
+let num4 = num2;
+let mod = num3 % num2;
+
+let max = Math.max(10, 20);
